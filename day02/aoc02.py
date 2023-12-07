5ec82862e3baabcd3ac226ddbd51fff374bd3edc
Christian Rosdahl <christian.rosdahl@gmail.com>
2023-12-07 22:54:56 +0100

    Add solution to day 2

---
+++ day02/aoc02.py
@@ -0,0 +1,64 @@
+# Advent of Code 2023 - day 2
+
+# %% Auxiliary functions
+def get_input():
+    file = open('input.txt','r')
+    lines = file.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace('\n', '')
+    return lines
+
+def set_to_dict(set):
+    dict = {}
+    for item in set.split(', '):
+        num, color = item.split(' ')
+        dict[color] = int(num)
+    return dict
+
+# %% Part 1
+def main(games):
+    feasible_game_id_sum = 0
+    actual_numbers = {'red': 12, 'green': 13, 'blue': 14}
+    for game in games:
+        game_possible = True
+        game_name, games = game.split(': ')
+        game_id = int(game_name.split(' ')[1])
+        sets = games.split('; ')
+        for set in sets:
+            if not possible_set(set_to_dict(set), actual_numbers):
+                game_possible = False
+                break
+        if game_possible:
+            feasible_game_id_sum += game_id
+    print(f'Result: {feasible_game_id_sum}')
+
+def possible_set(set, actual_numbers):
+    for color in actual_numbers:
+        if color in set and actual_numbers[color] < set[color]:
+            return False
+    return True
+
+games = get_input()
+main(games)
+
+# %% Part 2
+def main(games):
+    sum_of_powers = 0
+    for game in games:
+        min_num_cubes = {'red': 0, 'green': 0, 'blue': 0}
+        game_name, games = game.split(': ')
+        sets = games.split('; ')
+        for set in sets:
+            set_dict = set_to_dict(set)
+            for color in set_dict:
+                num_cubes_for_color = set_dict[color]
+                if num_cubes_for_color > min_num_cubes[color]:
+                    min_num_cubes[color] = num_cubes_for_color
+        power = 1;
+        for color in min_num_cubes:
+            power *= min_num_cubes[color]
+        sum_of_powers += power
+    print(f'Result: {sum_of_powers}')
+
+games = get_input()
+main(games)
