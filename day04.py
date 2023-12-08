7801c99c4f6e2203a654fd49bf8e6cd9534b1263
Christian Rosdahl <christian.rosdahl@gmail.com>
2023-12-08 22:14:48 +0100

    Add solution to day 4

---
+++ day04.py
@@ -0,0 +1,50 @@
+# Advent of Code 2023 - day 4
+
+# %% Common functions and classes (used in both parts)
+def get_input():
+    file = open('input04.txt','r')
+    lines = file.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace('\n', '')
+    file.close()
+    return lines
+
+def get_num_matches(card):
+    card = card.replace('  ', ' ') # Remove double spaces
+    card = card.split(': ')[1] # Remove "Card N: " from line
+    winning_nbrs, my_nbrs = card.split(' | ')
+    winning_nbrs = winning_nbrs.split(' ')
+    my_nbrs = my_nbrs.split(' ')
+
+    num_matches = 0
+    for nbr in my_nbrs:
+        if nbr in winning_nbrs:
+            num_matches += 1
+    return num_matches
+
+# %% Part 1
+def main():
+    cards = get_input()
+    answer = 0
+    for card in cards:
+        card_points = 0
+        num_matches = get_num_matches(card)
+        card_points = 2**(num_matches-1) if num_matches >= 1 else 0
+        answer += card_points
+    print(f'Answer: {answer}')
+
+main()
+
+# %% Part 2
+def main():
+    cards = get_input()
+    num_cards = [1 for _ in range(len(cards))]
+    for i, card in enumerate(cards):
+        num_matches = get_num_matches(card)
+        for j in range(i + 1, i + 1 + num_matches):
+            num_cards[j] += num_cards[i]
+
+    total_num_cards = sum(num_cards)
+    print(f'Answer: {total_num_cards}')
+
+main()
